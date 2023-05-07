1c65639f39132eb2afa668ec45486242342e19a6
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-05-07 22:45:15 +0530

    Add files via upload
    mko

---
+++ print.py
@@ -0,0 +1,15 @@
+# print("hello world")
+# loops
+# for i in range(0,5):
+#     print(i)
+# i=0
+# while i<5:
+#     print(i)
+#     i+=1
+i=0
+while i <4:
+    i+=1
+    print(i*'*')
+    
+for i in range(0,4):
+    print(i*"*")
