facf687ae548e242d5b1b6d9faf07f4774dde712
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-07 22:13:51 +0530

    Add files via upload
    MKJHIJ

---
+++ lists.py
@@ -0,0 +1,35 @@
+a=[3,5,6,74,54,30,'3k','jfdk']
+print(a[6])
+print(type(a[6]))
+print(a)
+a[6]=8
+print(a[6])
+
+
+b=[3,4,6,4,45,367,65,34]
+b=b.sort()
+
+print(b)
+
+print("hii")
+
+
+
+
+l1=[1,8,7,2,21,15]
+# print(l1)
+# l1_sorted=l1.sort()
+# print(l1_sorted)
+print(l1)
+# l1.sort()
+# l1.reverse()
+# l1.append(996776545)
+# l1.insert(3,8)
+# l1.pop(2 )
+# l1.remove(21)
+print(l1)
+
+
+
+
+
